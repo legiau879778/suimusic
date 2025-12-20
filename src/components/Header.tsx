7384eb8c0798e1bcea710078dd7325023a970fa6
legiau879778 <giaule.225167@gmail.com>
2025-12-20 17:24:48 +0700

    fix: add header.module.css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/context/AuthContext";
 import UserMenu from "./UserMenu";
-import styles from "@/styles/header.module.css";
+import styles from "src/styles/header.module.css";
 
 export default function Header() {
   const { user } = useAuth();
