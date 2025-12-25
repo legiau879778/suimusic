d6dee6bfe490f76a5d74f79d26f6776120cd58a9
legiau879778 <giaule.225167@gmail.com>
2025-12-25 22:41:37 +0700

    update types

--- src/types/qrcode.d.ts
+++ src/types/qrcode.d.ts
@@ -1,4 +1,26 @@
+// src/types/qrcode.d.ts
 declare module "qrcode" {
-  const QRCode: any;
+  export type QRCodeRenderersOptions = {
+    width?: number;
+    margin?: number;
+    errorCorrectionLevel?: "L" | "M" | "Q" | "H";
+    color?: {
+      dark?: string;
+      light?: string;
+    };
+  };
+
+  const QRCode: {
+    toCanvas: (
+      canvas: HTMLCanvasElement,
+      text: string,
+      options?: QRCodeRenderersOptions
+    ) => Promise<void>;
+
+    toDataURL: (text: string, options?: QRCodeRenderersOptions) => Promise<string>;
+
+    toString: (text: string, options?: any) => Promise<string>;
+  };
+
   export default QRCode;
 }
